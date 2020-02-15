bd5939eed5a66c949a128357c16ecdd73df93c71
SarahBechi <59558883+SarahBechi@users.noreply.github.com>
2020-02-15 09:48:55 +0100

    Updates

---
+++ precache-manifest.25006c6b03d7142d58f019a33eb93c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71abcda17c93ab96c0456205406c9b9",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "65829776e5fa32ae8c23",
+    "url": "/movie-app/static/css/main.cb8b4bde.chunk.css"
+  },
+  {
+    "revision": "94a4922e31c8a9b4d9a1",
+    "url": "/movie-app/static/js/2.485e3fd2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie-app/static/js/2.485e3fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65829776e5fa32ae8c23",
+    "url": "/movie-app/static/js/main.c778f20d.chunk.js"
+  },
+  {
+    "revision": "c48b9069213f4eaaa58a",
+    "url": "/movie-app/static/js/runtime-main.63067070.js"
+  }
+]);
